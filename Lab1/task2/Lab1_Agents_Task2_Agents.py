bcb4b021a414cee1d2d50ae76e16c7566771da71
Prateek Sohlot <prateeksohlot22@gmail.com>
2022-06-19 01:05:53 +0530

    Finished La1 Task2

---
+++ Lab1/task2/Lab1_Agents_Task2_Agents.py
@@ -0,0 +1,60 @@
+__author__ = 'Prateek Sohlot'
+
+from collections import namedtuple
+import random
+from Lab1_Agents_Task2_Hand import Hand
+from Lab1_Agents_Task2_Deck import Deck
+
+class randomAgent(Hand):
+    '''
+    Random Poker playing Agent
+    '''
+    def __init__(self, name):
+        super().__init__()   
+        self.name = name
+        self.type = 'Random'
+
+    def calculateBid(self):
+        return random.randint(1,50)
+
+    def __repr__(self) -> str:
+        f'{self.name}'
+
+class reflexAgent(Hand):
+    '''
+    Reflex Poker playing Agent
+    '''
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.type = 'Reflex'
+
+    def calculateBid(self):       
+        amount = (self.sameRank(self.hand[0]) - 1)*19 + self.ranks.index(self.hand[0].rank)-2
+        return amount
+
+class fixedAgent(Hand):
+    '''
+    Fixed Poker playing Agent
+    '''
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.stepValue = 1
+        self.type = 'Fixed'
+    
+    def calculateBid(self):
+        amount = 9 * self.stepValue
+        self.stepValue +=1
+        if self.stepValue == 5:
+            self.stepValue = 1
+        return amount
+
+
+
+
+
--- Lab1/task2/Lab1_Agents_Task2_PokerGame.py
+++ Lab1/task2/Lab1_Agents_Task2_PokerGame.py
@@ -1,30 +1,91 @@
 __author__ = 'Prateek Sohlot'
 
-from collections import namedtuple
-import random
+import json
+from Lab1_Agents_Task2_Deck import Deck
+from Lab1_Agents_Task2_Agents import randomAgent, reflexAgent, fixedAgent
+
+
+def play_game(player1, player2):
+    bids = {player1.name: [],player2.name: [], 'winning':[]}
+
+    # we play 50 rounds
+    for _ in range(50):
+
+        player1.updateHand(Deck().deal_cards())
+        player2.updateHand(Deck().deal_cards())
+
+        total = 0
+        # we have 3 rounds of bidding
+        for idx in range(3):
+            print(f'Staring round {idx+1}')
+            bid1 = player1.calculateBid()
+            print(f'{player1.name} bid {bid1}')
+            bid2 = player2.calculateBid()
+            print(f'{player2.name} bid {bid2}', '\n')            
+            total += bid1 + bid2
+
+        #########################
+        # phase 2:  Showdown and Hand Analysis   #
+        #########################
+        winner = ' '
+
+        print('*'*50)
+        print("Analysing player1's hand :", player1.hand)
+        analysis1 = player1.identifyHand()
+        print(f'{player1.name} has {analysis1["category"]}')
+        print(f'{player1.name} has {sorted(analysis1["rank"], reverse = True)}')
+        print('*'*50)
+
+        print('*'*50)
+        print("Analysing player2's hand :", player2.hand)
+        analysis2 = player2.identifyHand()
+        print(f'{player2.name} has {analysis2["category"]}')
+        print(f'{player2.name} has {sorted(analysis2["rank"], reverse= True)}')
+        print('*'*50)
+
+        if analysis1["cards"][0][2] > analysis2["cards"][0][2]:
+            winner = player1.name
+        elif analysis1["cards"][0][2] < analysis2["cards"][0][2]:
+            winner = player2.name
+
+        else:
+            
+            if sorted(analysis1['rank'])[0][2] > sorted(analysis2['rank'])[0][2]:
+                winner = player1.name
+
+            else:
+                winner = player2.name
+
+        print('*'*50)
+        print("The winner is:", winner)
+        print('*'*50)
+
+        if winner == player1.name:
+            bids[player1.name].append(total)
+            bids[player2.name].append(0)
+        elif winner == player2.name: 
+            bids[player1.name].append(0) 
+            bids[player2.name].append(total)
+
+    print('*'*50)
+    print(f'{player1.name} is a {player1.type}')     
+    print(f'The number of games won by {player1.name} are {len(list(filter(None, bids[player1.name])))}')
+    print("Total winning bids for player1:", sum(bids[player1.name]))
+    print(f'{player2.name} is a {player2.type}')
+    print(f'The number of games won by {player2.name} are {len(list(filter(None, bids[player2.name])))}')
+    print("Total winning bids for player2:", sum(bids[player2.name]))
+
+    return bids
 
 
 
+if __name__ == '__main__':
 
+    player1 = fixedAgent('Prateek')
+    player2 = randomAgent('Yuntao')
 
-#########################
-#      Game flow        #
-#########################
-
-
-#########################
-# phase 1: Card Dealing #
-#########################
-
-
-#########################
-# phase 2:   Bidding    #
-#########################
-
-
-#########################
-# phase 2:   Showdown   #
-#########################
-
-
-
+    result = play_game(player1, player2)
+    
+    print(player1.type, 'vs', player2.type)
+    with open(f'{player1.type}Vs{player2.type}.json', "w") as outfile:
+        json.dump(result, outfile)
